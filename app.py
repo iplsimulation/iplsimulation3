0f45b61cdfd9f70fb67c0d7cadd4c759d971e5ae
iplsimulation <82527308+iplsimulation@users.noreply.github.com>
2021-04-16 00:28:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 import streamlit as st
 import gspread
 import pandas as pd
+from pytz import timezone
 from fuzzywuzzy import process
-from datetime import date
+from datetime import date, datetime
 from oauth2client.service_account import ServiceAccountCredentials
 
 PAGE_CONFIG = {'page_title': 'IPL Simulation', 'page_icon': '📓', 'layout': 'centered'}
@@ -16,6 +17,7 @@ date_subject = {'20/04/2021': 'Big Data', '22/04/2021': 'Deep Learning', '24/04/
                '14/04/2021': 'Test', '15/04/2021': 'Test', '16/04/2021': 'Test', '17/04/2021': 'Test', '18/04/2021': 'Test', '19/04/2021': 'Test'}
 subject_id = {'Test': 0, 'Big Data': 1, 'Deep Learning': 2, 'Marketing Management': 3, 'Computer Vision': 4, 'Financial Institutions & Markets': 5, 'Natural Language Processing': 6}
 today = date.today().strftime('%d/%m/%Y')
+IST = timezone('Asia/Kolkata')
 
 def insertQA(subj, q, a):
     records = sheet.get_worksheet(subject_id[subj])
@@ -34,41 +36,71 @@ def insertQA(subj, q, a):
         st.subheader(f'Answer: {a}')
         st.subheader('Added!')
 
-def findQA(subj, q):
-    records = sheet.get_worksheet(subject_id[subj])
-    data = records.get_all_records()
-    questions = pd.DataFrame.from_dict(data)['Question'].tolist()
-    present = 0
-    for dt in data:
-        if dt['Question'] == q:
-            present += 1
-            st.subheader(f'Question: {q}')
-            st.subheader(f'Answer: {dt["Answer"]}')
-    
-    if present == 0:
-        st.subheader('Not found, two most similar questions are:')
-        qs = process.extract(q, questions)
-        q1, q2 = qs[0][0], qs[1][0]
+def findQA(subj, q, ret=False):
+    if not ret:
+        records = sheet.get_worksheet(subject_id[subj])
+        data = records.get_all_records()
+        questions = pd.DataFrame.from_dict(data)['Question'].tolist()
+        present = 0
         for dt in data:
-            if dt['Question'] == q1:
-                st.subheader(f'Question: {q1}')
+            if dt['Question'] == q:
+                present += 1
+                st.subheader(f'Question: {q}')
                 st.subheader(f'Answer: {dt["Answer"]}')
-            if dt['Question'] == q2:
-                st.subheader(f'Question: {q2}')
-                st.subheader(f'Answer: {dt["Answer"]}')
+
+        if present == 0:
+            qs = process.extract(q, questions)
+            qs = [q for q, score in qs]
+            st.subheader('Not found, five most similar questions are:')
+            q_, a_ = findQA(subj, qs, ret=True)
+            for i in range(5):
+                st.write(f'Question {i+1}: {q_[i]}')
+                st.write(f'Answer {i+1}: {a_[i]}')
+                st.write('-'*30)
         
+    elif ret:
+        records = sheet.get_worksheet(subject_id[subj])
+        data = records.get_all_records()
+        questions = pd.DataFrame.from_dict(data)['Question'].tolist()
+        q_, a_ = [], []
+        for dt in data:
+            if dt['Question'] in q:
+                q_.append(dt['Question'])
+                a_.append(dt['Answer'])
+        return q_, a_
+                
+def authenticate(u, p):
+    records = sheet.get_worksheet(7)
+    data = records.get_all_records()
+    for user in data:
+        if user['Username'] == u:
+            if user['Password'] == p:
+                return True
+    return False
+
+def log(t, u, s, q, a):
+    logs = sheet.get_worksheet(8)
+    logs.append_row([t, u, s, q, a])
 
 def main():
     st.header('IPL Simulation')
-    subj = date_subject[today]
-    question = st.text_input('Enter question:').strip().lower()
-    answer = st.text_input('Enter answer:').strip().lower()
-
-    if st.button('Submit'):
-        if answer == '' or answer == None:
-            findQA(subj, question)
+    username = st.sidebar.text_input('Username:')
+    password = st.sidebar.text_input('Password:', type='password')
+    if st.sidebar.checkbox('Login'):
+        if authenticate(username, password):
+            st.success(f'Logged in as {username.title()}')
+            subj = date_subject[today]
+            question = st.text_input('Enter question:').strip().lower()
+            answer = st.text_input('Enter answer:').strip().lower()
+            if st.button('Submit'):
+                if answer == '' or answer == None:
+                    findQA(subj, question)
+                    log(datetime.now(IST).strftime('%Y:%m:%d %H:%M:%S %Z %z'), username, subj, question, 'Find')
+                else:
+                    insertQA(subj, question, answer)
+                    log(datetime.now(IST).strftime('%Y:%m:%d %H:%M:%S %Z %z'), username, subj, question, 'Insert')
         else:
-            insertQA(subj, question, answer)
+            st.warning('Incorrect Username/Password')
         
 if __name__ == '__main__':
     main()
